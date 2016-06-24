0e1fa7d77c3203e0bbd110231066712234c4c23b
mawei <2191866109@qq.com>
2016-06-24 17:55:07 +0800

    添加spring初始化data 添加分类、fremaker

--- Work_Test/src/main/java/com/mw/java/test/ListTest.java
+++ Work_Test/src/main/java/com/mw/java/test/ListTest.java
@@ -9,10 +9,13 @@ import java.util.List;
 public class ListTest {
     public static void main(String[] args) {
         List s = new ArrayList();
-        s.add(1);
+        s.add(3);
         s.add(2);
-//        s.add(3);
-        System.out.println(s.size() == 2 ? s.get(1) : s.get(2));
+        s.add(1);
+//        System.out.println(s.size() == 2 ? s.get(1) : s.get(2));
+        System.out.println(s.toString());
+        System.out.println(s.subList(0,2).toString());
+        System.out.println(s.subList(0,1).toString());
     }
 
      /*传进去的只能是3或者2*/
@@ -21,4 +24,5 @@ public class ListTest {
 //    } else {//为空或者只有一级
 //        carParts.setCategoryId(Constant.EMPTY);
 //    }
+
 }
